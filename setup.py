a89cbd3a48fbd57792585b5ce4d403d32d2c4d6e
a-zankov <artzankoff@gmail.com>
2021-08-16 15:34:01 +0300

    Creating project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(name='apitest',
+      version='1.0',
+      description='my first api test project',
+      author='Art Zankov',
+      author_email='artzankoff@gmail.com',
+      packages=find_packages()
+      )
